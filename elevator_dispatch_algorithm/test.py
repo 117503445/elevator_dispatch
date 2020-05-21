435112d3c514c09deac6ec0def2da2ed876afcc0
117503445 <t117503445@gmail.com>
2020-05-21 15:00:10 +0800

    修复了算法层实现的一些小问题,增加测试方法

--- elevator_dispatch_algorithm/test.py
+++ elevator_dispatch_algorithm/test.py
@@ -1,5 +1,5 @@
 from algorithm import *
-
+import 算法层实现
 # 测试算法
 
 
@@ -105,3 +105,70 @@ def test2():
     person_3.in_which_elevator = 1
 
     return [[elevator(6, 1), elevator(3, 1), elevator(3.5, 0)], [person_1, person_2, person_3, person_4, person_5]]
+
+
+def test3():
+    person_1 = person()
+    person_1.from_floor = 3
+    person_1.to_floor = 12
+    person_1.come_time = 1
+    person_1.current_floor = 3
+    person_1.in_which_elevator = 0
+    person_1.is_out = False
+
+    person_2 = person()
+    person_2.from_floor = 3
+    person_2.to_floor = 12
+    person_2.come_time = 1
+    person_2.current_floor = 3
+    person_2.in_which_elevator = 0
+    person_2.is_out = False
+
+    person_3 = person()
+    person_3.from_floor = 3
+    person_3.to_floor = 12
+    person_3.come_time = 1
+    person_3.current_floor = 3
+    person_3.in_which_elevator = 0
+    person_3.is_out = False
+
+    person_4 = person()
+    person_4.from_floor = 4
+    person_4.to_floor = 6
+    person_4.come_time = 6
+    person_4.current_floor = 3
+    person_4.in_which_elevator = 0
+    person_4.is_out = False
+
+    person_5 = person()
+    person_5.from_floor = 4
+    person_5.to_floor = 6
+    person_5.come_time = 6
+    person_5.current_floor = 3
+    person_5.in_which_elevator = 0
+    person_5.is_out = False
+
+    return 算法层实现.core_algorithm(7, [person_1, person_2, person_3, person_4, person_5])
+
+
+if __name__ == "__main__":
+    print('test2_is')
+    result2 = test2()
+    r2 = algorithm_result()
+    r2.elevators = result2[0]
+    r2.people = result2[1]
+    r2_json = json.dumps(r2, indent=4)
+    print(r2_json)
+
+    print('test3_is')
+    result3 = test3()
+    r3 = algorithm_result()
+    r3.elevators = result3[0]
+    r3.people = result3[1]
+    r3_json = json.dumps(r3, indent=4)
+    print(r3_json)
+
+    if r2_json == r3_json:
+        print('test success')
+    else:
+        print('test failed')
